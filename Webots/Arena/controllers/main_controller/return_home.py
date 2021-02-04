0971c192b8cf30275a8b0c32453a142e99f2a17e
Sheev13 <rochussent@gmail.com>
2021-02-04 18:11:47 +0000

    actually added *dysfunctional* gps controller

---
+++ Webots/Arena/controllers/main_controller/return_home.py
@@ -0,0 +1,21 @@
+""" controller to return robot to starting point."""
+
+from controller import Robot, GPS
+
+robot = Robot()
+
+ts = int(robot.getBasicTimeStep())
+MAX_SPEED = 5
+
+# Initialize gps
+gps = robot.getDevice("GPS")
+gps.enable(ts)
+
+# Initialize motors
+r_motor = robot.getDevice("wheel1")
+l_motor = robot.getDevice("wheel2")
+l_motor.setPosition(float('inf'))
+l_motor.setVelocity(0.0)
+r_motor.setPosition(float('inf'))
+r_motor.setVelocity(0.0)
+
