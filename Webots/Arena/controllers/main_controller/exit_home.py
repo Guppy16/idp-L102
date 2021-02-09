fbb9f7c41f9f7cd4bceffd478d59ba30ef506bd7
Akash Gupta <ag2118@cam.ac.uk>
2021-02-09 11:10:46 +0000

    modified exit_home

--- Webots/Arena/controllers/main_controller/exit_home.py
+++ Webots/Arena/controllers/main_controller/exit_home.py
@@ -5,25 +5,29 @@ tol = 1e-2
 
 def inside_home(gps_vals, HOME):
     """Returns true if robot pos is within home box"""
-    pos = np.array([gps_vals[0],-gps_vals[2]])
+    pos = np.array([gps_vals[0], gps_vals[2]])
     homeVec = HOME - pos
     return np.linalg.norm(homeVec) < 0.40
     print("Arrived home chief")
 
-def heading_dot_home(gps_vals, cps_vals, HOME):
-    """Return the dot product between the heading and the homeVec"""
-    pos = np.array([gps_vals[0],-gps_vals[2]])
-    homeVec = HOME - pos
-    if np.linalg.norm(homeVec) < tol:
+def heading_dot_pos(gps_vals, cps_vals, HOME):
+    """Return the dot product between the heading and the pos relative to home vector"""
+    pos = np.array([gps_vals[0], gps_vals[2]])
+    pos -= HOME
+    print("pos ", pos)
+    if np.linalg.norm(pos) < tol:
         return 0
-    homeVec /= np.linalg.norm(homeVec)
+    pos /= np.linalg.norm(pos)
+    
 
     heading = np.array([cps_vals[0], cps_vals[2]])
+    print("heading " ,heading)
     heading /= np.linalg.norm(heading)
+    
 
-    return np.dot(heading, homeVec)
+    return np.dot(heading, pos)
 
-def exit_home(gps_vals, cps_vals, l_motor, r_motor, HOME = np.array([1.0, 1.0]), MAX_SPEED=5):
+def exit_home(gps_vals, cps_vals, l_motor, r_motor, HOME = np.array([1.0, -1.0]), MAX_SPEED=5):
     HOME = np.array(HOME)
     # Reverse motors if inside home
     if inside_home(gps_vals, HOME):
@@ -35,4 +39,4 @@ def exit_home(gps_vals, cps_vals, l_motor, r_motor, HOME = np.array([1.0, 1.0]),
         r_motor.setVelocity(-0.2*MAX_SPEED)
     
     # Check if rotated 180
-    return not heading_dot_home(gps_vals, cps_vals, HOME) + 1 < tol
+    return not abs(heading_dot_pos(gps_vals, cps_vals, HOME) - 1) < tol
