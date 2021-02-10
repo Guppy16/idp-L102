bc4beb85b42874552554a909a868505408d2e5c3
Akash Gupta <ag2118@cam.ac.uk>
2021-02-10 00:38:34 +0000

    updated protos

--- Webots/Arena/libraries/robot_to_proto.py
+++ Webots/Arena/libraries/robot_to_proto.py
@@ -1,5 +1,5 @@
 # Script to convert a robot in a wbt file to a .proto file
-# NOTE: need to manually change name, baseColor, controller, (maybe) mass and density
+# NOTE: need to manually change: name (DONE: baseColor, controller, mass and density)
 import re
 
 path_to_wbt = "../worlds/IDP-rootg" + ".wbt"
@@ -15,16 +15,17 @@ text = "".join(f)
 robot_text = r.findall(text)[0]
 
 # Replace lines with IS parameters
-r = re.compile(r'translation (.*)')
-robot_text = r.sub('translation IS translation\n\trotation IS rotation', robot_text, count=1)
-
-# Replace mass with bodyMass and density = -1
+robot_text = re.sub(r'controller (.*)', 'controller IS controller', robot_text)
+robot_text = re.sub(r'baseColor (.*)', 'baseColor IS color', robot_text, count=1)
+robot_text = re.sub(r'translation (.*)', 'translation IS translation\n\trotation IS rotation', robot_text, count=1)
 
 with open(path_to_proto, 'w') as f:
     f.write(
-        "PROTO root_g [\n\
-            field SFVec3f    translation  1 0.15 -1\n\
-            field SFRotation rotation     0 1 0 0\n\
-            field SFFloat    bodyMass     1\n\
-        ]\n" +  "{\nRobot " + robot_text + "\n}"
+        'PROTO root_g [\n\
+            field SFVec3f    translation  1 0.15 1\n\
+            field SFRotation rotation     0 1 0 3.14159\n\
+            field SFColor    color        1 0 0\n\
+            field SFString   name         "root_g"\n\
+            field SFString   controller   "main_controller"\n\
+        ]\n' +  "{\nRobot " + robot_text + "\n}"
     )
