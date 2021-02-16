b49976fd4af18acec9fe71b16753f18caddfb77a
Akash Gupta <ag2118@cam.ac.uk>
2021-02-16 02:54:37 +0000

    modified block class

--- Webots/Arena/controllers/main_controller/block.py
+++ Webots/Arena/controllers/main_controller/block.py
@@ -1,19 +1,19 @@
 import numpy as np
 
 class Block():
-    def __init__(self, position, pickedUp, color="unknown"):
-        self.color = color
-        self.position = position
-        self.pickedUp = False
-    
-    def getColor(self):
-       return self.color
-    
-    def getPosition(self):
-       return self.position
+   def __init__(self, position, pickedUp=False, color=None):
+      self.color = color
+      self.position = position
+      self.pickedUp = pickedUp
 
-    def pickedUp(self):
-       return self.pickedUp
+   def getColor(self):
+      return self.color
 
-    def getColor(self):
-       return self.color
+   def getPosition(self):
+      return self.position
+
+   def pickedUp(self):
+      return self.pickedUp
+   
+   def __repr__(self):
+      return f"Block:\tCol: {str(self.color)}\tPos:{self.position[0]:.2f} {self.position[1]:.2f}\tpickedUp:{self.pickedUp}"
